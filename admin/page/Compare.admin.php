1a46985bf5fe9191fc582c7b5789ce3ad9d4dbc1
Huyh Huyhu <2509roblox@gmail.com>
2023-07-18 08:21:03 +0700

    3

--- admin/page/Compare.admin.php
+++ admin/page/Compare.admin.php
@@ -1,7 +1,14 @@
  <?php
 
     include __DIR__ .  '/../inc/Header.php';
+    $class = new compare();
+    $show = $class->show_compare();
 
+    if (isset($_GET['delid'])) {
+        $delid = $_GET['delid'];
+        $delete = $class->delete_compare($delid);
+        $show = $class->show_compare();
+    }
     ?>
 
  <div class="main-content app-content">
@@ -18,66 +25,54 @@
                      <ol class="breadcrumb mb-0">
                          <li class="breadcrumb-item"><a href="home.php">Tables</a></li>
                          <li class="breadcrumb-item active" aria-current="page">Compare Tables</li>
+
                      </ol>
                  </nav>
              </div>
          </div> <!-- Page Header Close -->
 
          <div class="row">
+             <?php
+                if (isset($delete)) {
+                    echo $delete;
+                }
+                ?>
+             <?php
+                if (isset($show)) {
+                    if ($show && $show->num_rows > 0) {
+                        $i = 0;
+                        while ($result = $show->fetch_assoc()) {
+                            # code...
+                ?>
              <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6"> <a href="javascript:void(0);">
-                     <div class="card custom-card bg-dark overlay-card text-fixed-white"> <img src="https://spruko.com/demo/ynex/dist/assets/images/media/media-73.jpg" class="card-img" alt="...">
+                     <div class="card custom-card bg-dark overlay-card text-fixed-white"> <img
+                             src="<?php echo 'data:image/png;base64,' . base64_encode($result['productimg']); ?>"
+                             class="card-img" alt="...">
                          <div class="card-img-overlay d-flex flex-column p-0 over-content-bottom">
-                             <div class="card-footer border-top-0">
-                                 <h6 class="fw-semibold mb-0 text-fixed-white">Nature</h6>
+                             <div class="remove " style="margin-left: 20px;">
+                                 <a onclick="return confirm('Xác nhận xóa')"
+                                     href="?delid=<?php echo $result['compareid']; ?>">
+                                     <button class="btn btn-sm btn-danger remove-item-btn" data-bs-toggle="modal"
+                                         data-bs-target="#deleteRecordModal">Remove</button>
+                                 </a>
+                             </div>
+                             <div class="card-footer border-top-0 flex" style="display: flex;">
+                                 <h6 class="fw-semibold mb-0 text-fixed-white"><?php echo $result['useremail']; ?> </h6>
                              </div>
                          </div>
                      </div>
-                 </a> </div>
-             <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6"> <a href="javascript:void(0);">
-                     <div class="card custom-card bg-dark overlay-card text-fixed-white"> <img src="https://spruko.com/demo/ynex/dist/assets/images/media/media-74.jpg" class="card-img" alt="...">
-                         <div class="card-img-overlay d-flex flex-column p-0 over-content-bottom">
-                             <div class="card-footer border-top-0">
-                                 <h6 class="fw-semibold mb-0 text-fixed-white">Sports</h6>
-                             </div>
-                         </div>
-                     </div>
-                 </a> </div>
-             <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6"> <a href="javascript:void(0);">
-                     <div class="card custom-card bg-dark overlay-card text-fixed-white"> <img src="https://spruko.com/demo/ynex/dist/assets/images/media/media-75.jpg" class="card-img" alt="...">
-                         <div class="card-img-overlay d-flex flex-column p-0 over-content-bottom">
-                             <div class="card-footer border-top-0">
-                                 <h6 class="fw-semibold mb-0 text-fixed-white">Food</h6>
-                             </div>
-                         </div>
-                     </div>
-                 </a> </div>
-             <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6"> <a href="javascript:void(0);">
-                     <div class="card custom-card bg-dark overlay-card text-fixed-white"> <img src="https://spruko.com/demo/ynex/dist/assets/images/media/media-76.jpg" class="card-img" alt="...">
-                         <div class="card-img-overlay d-flex flex-column p-0 over-content-bottom">
-                             <div class="card-footer border-top-0">
-                                 <h6 class="fw-semibold mb-0 text-fixed-white">Travel</h6>
-                             </div>
-                         </div>
-                     </div>
-                 </a> </div>
-             <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6"> <a href="javascript:void(0);">
-                     <div class="card custom-card bg-dark overlay-card text-fixed-white"> <img src="https://spruko.com/demo/ynex/dist/assets/images/media/media-77.jpg" class="card-img" alt="...">
-                         <div class="card-img-overlay d-flex flex-column p-0 over-content-bottom">
-                             <div class="card-footer border-top-0">
-                                 <h6 class="fw-semibold mb-0 text-fixed-white">Fashion</h6>
-                             </div>
-                         </div>
-                     </div>
-                 </a> </div>
-             <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6"> <a href="javascript:void(0);">
-                     <div class="card custom-card bg-dark overlay-card text-fixed-white"> <img src="https://spruko.com/demo/ynex/dist/assets/images/media/media-78.jpg" class="card-img" alt="...">
-                         <div class="card-img-overlay d-flex flex-column p-0 over-content-bottom">
-                             <div class="card-footer border-top-0">
-                                 <h6 class="fw-semibold mb-0 text-fixed-white">Animals</h6>
-                             </div>
-                         </div>
-                     </div>
-                 </a> </div>
+                 </a>
+
+             </div>
+             <?php
+                            $i++;
+                        }
+                    } else {
+                        ?>
+             <?php
+                    }
+                }
+                ?>
          </div>
 
      </div>
@@ -86,7 +81,13 @@
      <div class="modal-dialog">
          <div class="modal-content">
              <div class="modal-body">
-                 <div class="input-group"> <a href="javascript:void(0);" class="input-group-text" id="Search-Grid"><i class="fe fe-search header-link-icon fs-18"></i></a> <input type="search" class="form-control border-0 px-2" placeholder="Search" aria-label="Username"> <a href="javascript:void(0);" class="input-group-text" id="voice-search"><i class="fe fe-mic header-link-icon"></i></a> <a href="javascript:void(0);" class="btn btn-light btn-icon" data-bs-toggle="dropdown" aria-expanded="false"> <i class="fe fe-more-vertical"></i> </a>
+                 <div class="input-group"> <a href="javascript:void(0);" class="input-group-text" id="Search-Grid"><i
+                             class="fe fe-search header-link-icon fs-18"></i></a> <input type="search"
+                         class="form-control border-0 px-2" placeholder="Search" aria-label="Username"> <a
+                         href="javascript:void(0);" class="input-group-text" id="voice-search"><i
+                             class="fe fe-mic header-link-icon"></i></a> <a href="javascript:void(0);"
+                         class="btn btn-light btn-icon" data-bs-toggle="dropdown" aria-expanded="false"> <i
+                             class="fe fe-more-vertical"></i> </a>
                      <ul class="dropdown-menu">
                          <li><a class="dropdown-item" href="#">Action</a></li>
                          <li><a class="dropdown-item" href="#">Another action</a></li>
@@ -98,13 +99,29 @@
                      </ul>
                  </div>
                  <div class="mt-4">
-                     <p class="font-weight-semibold text-muted mb-2">Are You Looking For...</p><span class="search-tags"><i class="fe fe-user me-2"></i>People<a href="javascript:void(0)" class="tag-addon"><i class="fe fe-x"></i></a></span> <span class="search-tags"><i class="fe fe-file-text me-2"></i>Pages<a href="javascript:void(0)" class="tag-addon"><i class="fe fe-x"></i></a></span> <span class="search-tags"><i class="fe fe-align-left me-2"></i>Articles<a href="javascript:void(0)" class="tag-addon"><i class="fe fe-x"></i></a></span> <span class="search-tags"><i class="fe fe-server me-2"></i>Tags<a href="javascript:void(0)" class="tag-addon"><i class="fe fe-x"></i></a></span>
+                     <p class="font-weight-semibold text-muted mb-2">Are You Looking For...</p><span
+                         class="search-tags"><i class="fe fe-user me-2"></i>People<a href="javascript:void(0)"
+                             class="tag-addon"><i class="fe fe-x"></i></a></span> <span class="search-tags"><i
+                             class="fe fe-file-text me-2"></i>Pages<a href="javascript:void(0)" class="tag-addon"><i
+                                 class="fe fe-x"></i></a></span> <span class="search-tags"><i
+                             class="fe fe-align-left me-2"></i>Articles<a href="javascript:void(0)" class="tag-addon"><i
+                                 class="fe fe-x"></i></a></span> <span class="search-tags"><i
+                             class="fe fe-server me-2"></i>Tags<a href="javascript:void(0)" class="tag-addon"><i
+                                 class="fe fe-x"></i></a></span>
                  </div>
                  <div class="my-4">
                      <p class="font-weight-semibold text-muted mb-2">Recent Search :</p>
-                     <div class="p-2 border br-5 d-flex align-items-center text-muted mb-2 alert"> <a href="notifications.html"><span>Notifications</span></a> <a class="ms-auto lh-1" href="javascript:void(0);" data-bs-dismiss="alert" aria-label="Close"><i class="fe fe-x text-muted"></i></a> </div>
-                     <div class="p-2 border br-5 d-flex align-items-center text-muted mb-2 alert"> <a href="alerts.html"><span>Alerts</span></a> <a class="ms-auto lh-1" href="javascript:void(0);" data-bs-dismiss="alert" aria-label="Close"><i class="fe fe-x text-muted"></i></a> </div>
-                     <div class="p-2 border br-5 d-flex align-items-center text-muted mb-0 alert"> <a href="mail.html"><span>Mail</span></a> <a class="ms-auto lh-1" href="javascript:void(0);" data-bs-dismiss="alert" aria-label="Close"><i class="fe fe-x text-muted"></i></a> </div>
+                     <div class="p-2 border br-5 d-flex align-items-center text-muted mb-2 alert"> <a
+                             href="notifications.html"><span>Notifications</span></a> <a class="ms-auto lh-1"
+                             href="javascript:void(0);" data-bs-dismiss="alert" aria-label="Close"><i
+                                 class="fe fe-x text-muted"></i></a> </div>
+                     <div class="p-2 border br-5 d-flex align-items-center text-muted mb-2 alert"> <a
+                             href="alerts.html"><span>Alerts</span></a> <a class="ms-auto lh-1"
+                             href="javascript:void(0);" data-bs-dismiss="alert" aria-label="Close"><i
+                                 class="fe fe-x text-muted"></i></a> </div>
+                     <div class="p-2 border br-5 d-flex align-items-center text-muted mb-0 alert"> <a
+                             href="mail.html"><span>Mail</span></a> <a class="ms-auto lh-1" href="javascript:void(0);"
+                             data-bs-dismiss="alert" aria-label="Close"><i class="fe fe-x text-muted"></i></a> </div>
                  </div>
              </div>
              <div class="modal-footer">
--- classes/Compare.class.php
+++ classes/Compare.class.php
@@ -13,19 +13,88 @@ class compare
         $this->db = new Database();
         $this->fm = new Format();
     }
-    public function insert_user($data, $file)
+    public function insert_compare($data)
     {
+        $compareuserid = mysqli_real_escape_string($this->db->link, $data['compareuserid']);
+        $compareproductid = mysqli_real_escape_string($this->db->link, $data['compareproductid']);
+
+        if (empty($compareuserid) || empty($compareproductid)) {
+            $arlet = "<div class='alert alert-danger' role='alert'>Code must not be empty</div>";
+            return $arlet;
+        } else {
+            $check_register = "SELECT *
+        FROM tbl_compares WHERE compareuserid = '$compareuserid' AND compareproductid = '$compareproductid'   LIMIT 1";
+            $check = $this->db->select($check_register);
+
+            if (isset($check)) {
+                if ($check->num_rows > 0) {
+                    $arlet = "<div class='alert alert-danger' role='alert'>Compare exists</div>";
+                    return $arlet;
+                } else {
+                    $query = "INSERT INTO tbl_compares(compareuserid,compareproductid) VALUES ('$compareuserid','$compareproductid')";
+                    $result = $this->db->insert($query);
+                    if ($result) {
+                        $arlet = "<div class='alert alert-success' role='alert'>Insert Code Successfully</div>";
+                        return $arlet;
+                    } else {
+                        $arlet = "<div class='alert alert-danger' role='alert'>Insert Code Failed</div>";
+                        return $arlet;
+                    }
+                }
+            } else {
+            }
+        }
     }
 
-    public function show_user()
+    public function show_compare()
     {
+        $query = "SELECT tbl_compares.*, tbl_products.* ,tbl_users.*   
+        FROM tbl_compares
+        INNER JOIN tbl_products ON tbl_compares.compareproductid = tbl_products.productid
+        INNER JOIN tbl_users ON tbl_compares.compareuserid = tbl_users.userid
+        ORDER BY tbl_compares.compareid DESC;";
+        $result = $this->db->select($query);
+
+        return $result;
     }
 
 
-    public function update_user($data, $id)
+    public function update_compare($data, $id)
     {
+        $compareuserid = mysqli_real_escape_string($this->db->link, $data['compareuserid']);
+        $compareproductid = mysqli_real_escape_string($this->db->link, $data['compareproductid']);
+        $id = mysqli_real_escape_string($this->db->link, $id);
+
+        if ($compareuserid == '' || $compareproductid == '') {
+            $arlet = "<div class='alert alert-danger' role='alert'>Category name empty</div>";
+            return $arlet;
+        } else {
+            $query = "UPDATE tbl_compares SET compareuserid = '$compareuserid', compareproductid = '$compareproductid' WHERE compareid = '$id'";
+            $result = $this->db->update($query);
+            if ($result) {
+                $arlet = "<div class='alert alert-success' role='alert'>Update Category Successfully</div>";
+                return $arlet;
+            } else {
+                $arlet = "<div class='alert alert-danger' role='alert'>Error</div>";
+
+                return $arlet;
+            }
+        }
     }
-    public function delete_user($id)
+    public function delete_compare($id)
     {
+        $id = mysqli_real_escape_string($this->db->link, $id);
+        $query = "DELETE FROM tbl_compares WHERE compareid = '$id'";
+        $result = $this->db->delete($query);
+
+
+        if ($result) {
+            $arlet = "<div class='alert alert-success' role='alert'>Delete Code Successfully</div>";
+            return $arlet;
+        } else {
+            $arlet = "<div class='alert alert-danger' role='alert'>Delete Code Successfully</div>";
+
+            return $arlet;
+        }
     }
 }
