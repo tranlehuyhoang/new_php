043820fc627e3ce60b10c28c10e9e7dfcf6ab66f
Huyh Huyhu <2509roblox@gmail.com>
2023-07-21 07:17:44 +0700

    12

--- page/Page404.php
+++ page/Page404.php
@@ -12,11 +12,15 @@ include '../inc/Header.php';
                         <p>Sorry but the page you are looking for does not exist, have been removed, name changed or is
                             temporarity unavailable.</p>
                         <form action="#" class="search-form search-form--3 mb--60">
-                            <input type="text" name="search" id="error_search" placeholder="Search..." class="search-form__input">
+                            <input type="text" name="search" id="error_search" placeholder="Search..."
+                                class="search-form__input">
                             <button type="submit" class="search-form__submit">
                                 <i class="fa fa-search"></i>
                             </button>
                         </form>
+
+
+
                         <a href="page/home.php" class="btn btn-4 btn-style-3">Back to home page</a>
                     </div>
                 </div>
